17f252ac95fc34fe5f7af688ed5a96805a0e346a
MITESHDABHI <dabhimitesh18@gmail.com>
2023-03-02 13:01:43 +0000

    change added

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app,host="localhost")
+    socketio.run(app,host="localhost",allow_unsafe_werkzeug=True)
